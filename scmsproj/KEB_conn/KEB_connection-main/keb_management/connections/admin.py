619c71bd26c2118a519fe4fccac26cd3a7fa4c52
Shreyansh200404 <shreyanshmurabatte12@gmail.com>
2025-05-11 03:25:47 +0530

    commit

--- scmsproj/KEB_conn/KEB_connection-main/keb_management/connections/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Customer, Connection, ContactMessage
-
-admin.site.register(Customer)
-admin.site.register(Connection)
-admin.site.register(ContactMessage)
